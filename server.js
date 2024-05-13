3bfc9161507d1ec8f3a05384f1d544a4dca3a125
bentan4853563 <bentan010918+1@gmai.com>
2024-05-13 17:31:55 +0300

    fix server

--- server.js
+++ server.js
@@ -1,60 +1,17 @@
 // /* eslint-disable no-undef */
-// const path = require("path");
-// const fs = require("fs");
-// const https = require("https");
-// const express = require("express");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-// require("dotenv").config();
-
-// const options = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/lurny.net/privkey.pem", "utf8"),
-//   ca: fs.readFileSync("/etc/letsencrypt/live/lurny.net/fullchain.pem", "utf8"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/lurny.net/cert.pem", "utf8"),
-// };
-
-// const app = express();
-
-// connectDB();
-
-// app.use(express.json({ limit: "500mb" }));
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(cors());
-
-// // Redirect www.lurny.net to lurny.net
-// app.use((req, res, next) => {
-//   if (req.hostname === "www.lurny.net") {
-//     res.redirect(301, `https://lurny.net${req.url}`);
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use("/api/auth", require("./routes/api/authRouter"));
-// app.use("/api/user", require("./routes/api/userRouter"));
-// app.use("/api/lurny", require("./routes/api/lurnyRouter"));
-// app.use("/api/study", require("./routes/api/studyRouter"));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/dist"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-//   });
-// }
-
-// const PORT = process.env.PORT || 443;
-
-// https.createServer(options, app).listen(PORT, () => {
-//   console.log(`Server started on ${PORT}`);
-// });
-
-/* eslint-disable no-undef */
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
+
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/lurny.net/privkey.pem", "utf8"),
+  ca: fs.readFileSync("/etc/letsencrypt/live/lurny.net/fullchain.pem", "utf8"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/lurny.net/cert.pem", "utf8"),
+};
 
 const app = express();
 
@@ -64,6 +21,15 @@ app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+// Redirect www.lurny.net to lurny.net
+app.use((req, res, next) => {
+  if (req.hostname === "www.lurny.net") {
+    res.redirect(301, `https://lurny.net${req.url}`);
+  } else {
+    next();
+  }
+});
 
 app.use("/api/auth", require("./routes/api/authRouter"));
 app.use("/api/user", require("./routes/api/userRouter"));
@@ -77,8 +43,42 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 443;
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
+
+/* eslint-disable no-undef */
+// const path = require("path");
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+// require("dotenv").config();
+
+// const app = express();
+
+// connectDB();
+
+// app.use(express.json({ limit: "500mb" }));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cors());
+
+// app.use("/api/auth", require("./routes/api/authRouter"));
+// app.use("/api/user", require("./routes/api/userRouter"));
+// app.use("/api/lurny", require("./routes/api/lurnyRouter"));
+// app.use("/api/study", require("./routes/api/studyRouter"));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/dist"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+//   });
+// }
+
+// const PORT = process.env.PORT || 5005;
+
+// app.listen(PORT, () => {
+//   console.log(`Server started on ${PORT}`);
+// });
