1878adca9c195d244b6a27b10ec7723dc67c7943
bentan4853563 <bentan010918+1@gmai.com>
2024-05-13 18:07:36 +0300

    chagned haeder of lurny reminder

--- client/src/pages/LurnyRemind.jsx
+++ client/src/pages/LurnyRemind.jsx
@@ -1,30 +1,21 @@
 // import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 import { jwtDecode } from "jwt-decode";
 
-import { IoMenu } from "react-icons/io5";
-
-import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
-
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 
-import LetterLogo from "../assets/icons/letter_logo.png";
 import defaultImg from "../assets/images/Lurny/default.png";
 
-import { IoSearchSharp } from "react-icons/io5";
-
 import TranslateComponent from "../components/TranslateComponent";
-import { logout } from "../reducers/userSlice";
 import TestQuizItem from "../components/TestQuizItem";
+import Header from "../components/Header";
 
 function LurnyRemind() {
-  const dispatch = useDispatch();
-  const { lurnies } = useSelector((state) => state.lurny);
   const { studies } = useSelector((state) => state.study);
 
   const [userData, setUserData] = useState(null);
@@ -117,106 +108,7 @@ function LurnyRemind() {
 
   return (
     <div className="min-h-[100vh] bg-black font-raleway flex flex-col justify-center sm:justify-start">
-      {/* Header */}
-      <div className="w-full bg-black px-[4rem] sm:px-[20rem] flex justify-between items-center mb-[4rem] sm:mb-0 sm:py-[2rem]">
-        <Link to="/lurny/search" className="select-none">
-          <img
-            src={LetterLogo}
-            alt="Letter logo"
-            className="w-[56rem] sm:w-[32rem] md:w-[24rem] lg:w-[18rem] xl:w-[12rem]"
-          />
-        </Link>
-
-        <div className="flex items-center gap-[8rem] lg:gap-[2rem]">
-          {lurnies.length > 0 && (
-            <Link to={"/lurny/search"}>
-              <IoSearchSharp className="text-white text-[16rem] sm:text-[4rem] hover:text-gray-400" />
-            </Link>
-          )}
-          {userData && (
-            <Menu
-              menuButton={
-                <img
-                  src={userData.photoURL}
-                  alt="User avatar"
-                  className="w-[16rem] sm:w-[12rem] md:w-[10rem] lg:w-[8rem] xl:w-[4rem] rounded-[100%] cursor-pointer"
-                />
-              }
-              transition
-              gap={8}
-              align="center"
-            >
-              <MenuItem>
-                <Link
-                  to="/lurny/profile"
-                  className="px-[8rem] sm:px-[2rem] text-[8rem] sm:text-[1.5rem] text-black hover:text-black"
-                >
-                  Profile
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <span
-                  onClick={() => dispatch(logout())}
-                  className="px-[8rem] sm:px-[2rem] text-[8rem] sm:text-[1.5rem] text-black"
-                >
-                  Logout
-                </span>
-              </MenuItem>
-            </Menu>
-          )}
-          <div className="hidden sm:flex">
-            <Menu
-              menuButton={
-                <MenuButton>
-                  <IoMenu className="flex text-[4rem] text-gray-500" />
-                </MenuButton>
-              }
-              transition
-              gap={8}
-              align="center"
-            >
-              <MenuItem>
-                <Link
-                  to="/"
-                  className="w-full py-[0.5rem] text-black text-[1.5rem]"
-                >
-                  About
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <span className="w-full py-[0.5rem] text-black text-[1.5rem]">
-                  Terms of Service
-                </span>
-              </MenuItem>
-              <MenuItem>
-                <Link
-                  to="/privacy"
-                  className="w-full py-[0.5rem] text-black text-[1.5rem] hover:text-black"
-                >
-                  Privacy
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <Link
-                  to="/lurny/price"
-                  className="w-full py-[0.5rem] text-black text-[1.5rem] hover:text-black"
-                >
-                  Pricing
-                </Link>
-              </MenuItem>
-              <MenuItem className="hover:bg-white">
-                <span className="w-full py-[0.5rem] text-[1.5rem]">
-                  A&nbsp;
-                  <span className="text-[#7030A0] font-semibold">
-                    CarillonMedia
-                  </span>
-                  &nbsp;Company
-                </span>
-              </MenuItem>
-            </Menu>
-          </div>
-        </div>
-      </div>
+      <Header />
 
       {/* body */}
       <div className="flex flex-wrap px-[4rem] sm:px-[20rem] py-[2rem] gap-[8rem] sm:gap-0">
