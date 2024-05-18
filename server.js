fd56803473732ec4df52f2e3af0dee14a5b6992e
bentan4853563 <bentan010918+1@gmai.com>
2024-05-18 08:35:39 +0300

    changed server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // /* eslint-disable no-undef */
 const path = require("path");
 const fs = require("fs");
+const http = require("http");
 const https = require("https");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
+
+const app = express();
+
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 
 const options = {
   key: fs.readFileSync("/etc/letsencrypt/live/lurny.net/privkey.pem", "utf8"),
@@ -13,14 +19,26 @@ const options = {
   cert: fs.readFileSync("/etc/letsencrypt/live/lurny.net/cert.pem", "utf8"),
 };
 
-const app = express();
-
 connectDB();
 
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+// Redirect from HTTP to HTTPS
+const httpApp = express();
+httpApp.get("*", (req, res) => {
+  res.redirect(`https://${req.headers.host}${req.url}`);
+});
+
+// Start HTTP server for redirection to HTTPS
+const httpServer = http.createServer(httpApp);
+httpServer.listen(HTTP_PORT, () => {
+  console.log(
+    `HTTP Server running on port ${HTTP_PORT} and redirecting to HTTPS`
+  );
+});
 
 // Redirect www.lurny.net to lurny.net
 app.use((req, res, next) => {
@@ -45,8 +63,9 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 443;
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
 });
 
 /* eslint-disable no-undef */
